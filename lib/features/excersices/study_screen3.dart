6d97e736d1e6cf3629e57494be87b55c379f324a
alejandro mota <aamotadev@gmail.com>
2023-07-13 11:04:52 -0600

    rectificar

--- lib/features/excersices/study_screen3.dart
+++ lib/features/excersices/study_screen3.dart
@@ -44,138 +44,126 @@ class _Study_Screen3State extends State<Study_Screen3> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: primaryBackground,
-        appBar: AppBar(
-          leading: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.arrow_back,
-                color: secondaryBackground,
-                size: 36,
-              )),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      backgroundColor: primaryBackground,
+      appBar: AppBar(
+        forceMaterialTransparency: true,
+        leading: IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.arrow_back,
+              color: secondaryBackground,
+              size: 36,
+            )),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            LinearPercentIndicator(
+              width: 290.0,
+              lineHeight: 10.0,
+              percent: 0.5,
+              backgroundColor: secondaryBackground,
+              progressColor: checkColor,
+              barRadius: const Radius.circular(60.00),
+            ),
+          ],
+        ),
+      ),
+      body: Material(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              LinearPercentIndicator(
-                width: 290.0,
-                lineHeight: 10.0,
-                percent: 0.5,
-                backgroundColor: secondaryBackground,
-                progressColor: checkColor,
-                barRadius: Radius.circular(60.00),
+              const Text(
+                "Fill in the gaps",
+                style: TextStyle(
+                    color: secondaryBackground,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800),
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  isPlaying
+                      ? IconButton(
+                          onPressed: () {
+                            player.pause();
+                          },
+                          icon: SvgPicture.asset(
+                            'assets/playButton.svg',
+                            width: 70.0,
+                            height: 70.0,
+                          ),
+                        )
+                      : IconButton(
+                          onPressed: () {
+                            player.play(
+                                UrlSource('https://example.com/audio.mp3'));
+                          },
+                          icon: SvgPicture.asset(
+                            'assets/playButton.svg',
+                            width: 70.0,
+                            height: 70.0,
+                          ),
+                        ),
+                  Slider(
+                    min: 0,
+                    max: duration.inSeconds.toDouble(),
+                    value: position.inSeconds.toDouble(),
+                    onChanged: (value) {
+                      final position = Duration(seconds: value.toInt());
+                      player.seek(position);
+                      player.resume();
+                    },
+                  ),
+                ],
+              ),
+              const Text(
+                "What kind of",
+                style: TextStyle(fontWeight: FontWeight.w600, color: blackTextColor),
+              ),
+              Container(
+                width: 350,
+                height: 50,
+                decoration: BoxDecoration(
+                    color: buttonColor,
+                    borderRadius: BorderRadius.circular(20),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          spreadRadius: 1,
+                          blurRadius: 1,
+                          offset: const Offset(1, 4))
+                    ]),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: TextField(
+                    textAlign: TextAlign.left,
+                    decoration: InputDecoration(
+                      enabledBorder: InputBorder.none,
+                      fillColor: Colors.grey,
+                      hintStyle: TextStyle(
+                          color: Colors.grey.withOpacity(
+                              0.5)), // Agrega la opacidad deseada al color
+                      hintText: 'type what your heard',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const Text("is that",
+                  style: TextStyle(fontWeight: FontWeight.w800, color: blackTextColor)),
+              ButtonConstant(
+                  widthSize: 320,
+                  heightSize: 60,
+                  backgroundColor: checkColor,
+                  text: "Check",
+                  color: blackTextColor)
             ],
           ),
         ),
-        body: SingleChildScrollView(
-          child: Material(
-              child: Center(
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 80),
-                  child: Text(
-                    "Fill in the gaps",
-                    style: TextStyle(
-                        color: secondaryBackground,
-                        fontSize: 24,
-                        fontWeight: FontWeight.w800),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 100, left: 60),
-                  child: Row(
-                    children: [
-                      isPlaying
-                          ? IconButton(
-                              onPressed: () {
-                                player.pause();
-                              },
-                              icon: SvgPicture.asset(
-                                'assets/playButton.svg',
-                                width: 70.0,
-                                height: 70.0,
-                              ),
-                            )
-                          : IconButton(
-                              onPressed: () {
-                                player.play(
-                                    UrlSource('https://example.com/audio.mp3'));
-                              },
-                              icon: SvgPicture.asset(
-                                'assets/playButton.svg',
-                                width: 70.0,
-                                height: 70.0,
-                              ),
-                            ),
-                      Slider(
-                        min: 0,
-                        max: duration.inSeconds.toDouble(),
-                        value: position.inSeconds.toDouble(),
-                        onChanged: (value) {
-                          final position = Duration(seconds: value.toInt());
-                          player.seek(position);
-                          player.resume();
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text("What kind of"),
-                ),
-                SingleChildScrollView(
-                  child: Container(
-                    width: 600,
-                    height: 70,
-                    child: Padding(
-                        padding: EdgeInsets.all(10),
-                        child: TextField(
-                            textAlign: TextAlign.left,
-                            decoration: InputDecoration(
-                                fillColor: Colors.grey,
-                                hintStyle: TextStyle(
-                                    color: Colors.grey.withOpacity(
-                                        0.5)), // Agrega la opacidad deseada al color
-                                hintText: 'type what your heard',
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                )))),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text("is that"),
-                ),
-                Padding(
-                    padding: EdgeInsets.only(top: 80),
-                    child: Container(
-                      width: 320,
-                      height: 60,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              Color.fromARGB(255, 115, 225,
-                                  119)), // Set the background color to green
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(
-                                  7), // Adjust the corner radius as desired
-                            ),
-                          ),
-                        ),
-                        onPressed: () {},
-                        child: const Text(
-                          "Check",
-                          style: TextStyle(fontWeight: FontWeight.w400),
-                        ),
-                      ),
-                    ))
-              ],
-            ),
-          )),
-        ));
+      ),
+    );
   }
 }
--- lib/utils/contants.dart
+++ lib/utils/contants.dart
@@ -7,10 +7,58 @@ const secondaryBackground = Color(0xff906CF3);
 const primaryTextColor = Color(0xff4E4E4E);
 const blackTextColor= Color(0xff4A4A4A);
 const buttonColor = Color(0xffFFF6FF);
-const checkColor = Color.fromRGBO(168, 255, 187, 100);
+const checkColor = Color(0xffA8FFBB);
 
 
 const darkmodebutton = Color(0xff906cf3);
 const darkmodeTextColor = Color(0xffFFFFFF);
 const darkmodeprimarycolor = Color(0xff000000);
 const darkmodeSecondarycolor = Color(0xffFFFFFF);
+
+
+class ButtonConstant extends StatelessWidget {
+  //
+  ButtonConstant({
+    required this.widthSize,
+    required this.heightSize,
+    required this.backgroundColor,
+    required this.text,
+    required this.color
+}
+  );
+
+  final double widthSize;
+  final double heightSize;
+  final Color backgroundColor;
+  final String text;
+  final Color color;
+
+
+  Widget build(BuildContext context) {
+    return Container(
+      width: widthSize,
+      height: heightSize,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 1,
+                blurRadius: 1,
+                offset: const Offset(0, 4))
+          ]),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+            backgroundColor: backgroundColor,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10))),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: TextStyle(
+              fontWeight: FontWeight.w800, color: color),
+        ),
+      ),
+    );
+  }
+}
